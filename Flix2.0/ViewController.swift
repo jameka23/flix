dc105547a566d7d535e1224da3c337d1b3726700
Jameka Echols <jameka.echols@gmail.com>
2021-02-16 18:20:35 -0600

    Initial Commit

---
+++ Flix2.0/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Flix2.0
+//
+//  Created by Jameka Echols on 2/16/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
