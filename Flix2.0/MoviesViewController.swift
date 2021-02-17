e1109e86b9462a0be2125ffb0c1b6ff0efbb0e2d
Jameka Echols <jameka.echols@gmail.com>
2021-02-17 14:59:53 -0600

    .

--- Flix2.0/MoviesViewController.swift
+++ Flix2.0/MoviesViewController.swift
@@ -59,8 +59,10 @@ class MoviesViewController: UIViewController,UITableViewDataSource, UITableViewD
         let synopsis = movie["overview"] as? String
         
         let baseUrl = "https://image.tmdb.org/t/p/w185"
-        let posterPath = movie["poster_path"] as? String
+        let posterPath = movie["poster_path"] as! String
         let posterUrl = URL(string: baseUrl + posterPath)
+        
+        cell.posterView.af_setImage(withURL: posterUrl!)
         cell.titleLabel.text = title
         cell.synopsisLabel.text = synopsis
         
