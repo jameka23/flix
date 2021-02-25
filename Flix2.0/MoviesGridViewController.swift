02a61ce23240f57888d2983ee89e480a7589881a
Jameka Echols <jameka.echols@gmail.com>
2021-02-24 21:53:57 -0600

    dont to Estimate Size in the collections view while inside storyboard

--- Flix2.0/MoviesGridViewController.swift
+++ Flix2.0/MoviesGridViewController.swift
@@ -6,15 +6,30 @@
 //
 
 import UIKit
+import AlamofireImage
 
-class MoviesGridViewController: UIViewController {
+class MoviesGridViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate {
 
     
+    @IBOutlet var collectionsView: UICollectionView!
     var movies = [[String:Any]]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        
+        collectionsView.dataSource = self
+        collectionsView.delegate = self
+        
+        let layout = collectionsView.collectionViewLayout as! UICollectionViewFlowLayout
+        layout.minimumLineSpacing = 40 // controlss the space in between the rows ( 40px between row)
+        layout.minimumInteritemSpacing = 0
+        
+        let width = view.frame.size.width / 3
+        
+        
+        layout.itemSize = CGSize(width: width, height: width * 1.5)
+        
         let url = URL(string: "https://api.themoviedb.org/3/movie/297762/similar?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed")!
         let request = URLRequest(url: url, cachePolicy: .reloadIgnoringLocalCacheData, timeoutInterval: 10)
         let session = URLSession(configuration: .default, delegate: nil, delegateQueue: OperationQueue.main)
@@ -28,20 +43,34 @@ class MoviesGridViewController: UIViewController {
 
               // casting
             self.movies = dataDictionary["results"] as! [[String:Any]]
-            print(self.movies)
+            self.collectionsView.reloadData()
+            
+            
            }
         }
         task.resume()    }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return movies.count
     }
-    */
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "MovieGridCell", for: indexPath) as! MovieGridCell
+        
+        let movie = movies[indexPath.item]
+        
+        
+        let baseUrl = "https://image.tmdb.org/t/p/w185"
+        let posterPath = movie["poster_path"] as! String
+        let posterUrl = URL(string: baseUrl + posterPath)
+        
+        cell.posterView.af.setImage(withURL: posterUrl!)
+        
+        
+        return cell
+    }
+    
 
 }
